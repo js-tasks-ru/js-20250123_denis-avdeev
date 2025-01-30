58e55e04b621834dfb6af94cb679eb0249f90115
Avdeyev Denis <Avdeyev9424@gmail.com>
2025-01-30 04:33:05 +0300

    функция создаёт новый отсортированный массив

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,16 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  const tempArr = arr.slice().sort((a, b) => {
+    const comparison = a.localeCompare(b, ['ru', 'en'], { sensitivity: 'base' });
+    if (comparison === 0) {
+      return b.localeCompare(a, ['ru', 'en'], {sensitivity: 'case'});
+    }
+    return comparison;
+  });
+  if (param == 'asc') {
+    return tempArr;
+  } else if (param == 'desc') {
+    return tempArr.reverse();
+  }
 }
